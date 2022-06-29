52bb3a27266528c47490ad16a7d8921a0c81a79a
Abhijeet Srivastava <abhisri@udel.edu>
2022-06-29 22:13:23 +0530

    @EqualsAndHashcode

--- src/main/java/com/auth/model/AuthToken.java
+++ src/main/java/com/auth/model/AuthToken.java
@@ -1,6 +1,7 @@
 package com.auth.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,38 +16,8 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class AuthToken {
     String token;
     String user;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((token == null) ? 0 : token.hashCode());
-        result = prime * result + ((user == null) ? 0 : user.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        AuthToken other = (AuthToken) obj;
-        if (token == null) {
-            if (other.token != null)
-                return false;
-        } else if (!token.equals(other.token))
-            return false;
-        if (user == null) {
-            if (other.user != null)
-                return false;
-        } else if (!user.equals(other.user))
-            return false;
-        return true;
-    }
 }
--- src/main/java/com/auth/model/AuthTokenResponse.java
+++ src/main/java/com/auth/model/AuthTokenResponse.java
@@ -3,6 +3,7 @@ package com.auth.model;
 import org.springframework.http.HttpStatus;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,35 +13,8 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class AuthTokenResponse {
 	HttpStatus status;
     AuthToken token;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((status == null) ? 0 : status.hashCode());
-        result = prime * result + ((token == null) ? 0 : token.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        AuthTokenResponse other = (AuthTokenResponse) obj;
-        if (status != other.status)
-            return false;
-        if (token == null) {
-            if (other.token != null)
-                return false;
-        } else if (!token.equals(other.token))
-            return false;
-        return true;
-    }
 }
